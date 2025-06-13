527572d306d172ee26a279f7f4d2a1d4034ac7b7
Karl Parton <karlparton@gmail.com>
2025-06-13 15:13:59 +0100

    Prototype setup

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,16 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// Add your routes here
+router.use((req, res, next) => {
+  res.locals.query = req.query
+  res.locals.path = req.path
+  next()
+})
+
+// Sprint routes setup
+
+const sprint00 = require('./views/prototypes/sprint-00/_routes');
+router.use('/prototypes/sprint-00', sprint00);
+
+const sprint01 = require('./views/prototypes/sprint-01/_routes');
+router.use('/prototypes/sprint-01', sprint01);
