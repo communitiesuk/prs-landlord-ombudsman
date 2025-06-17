8065fc80528594f336f6c7eb123cb8cf6bb1c1ff
Lewis Morris <lewis@lewismorris.co.uk>
2025-06-17 18:25:31 +0100

    Add cookie check for branded/unbranded, add unbranded template and update main view to njk from html

--- app/routes.js
+++ app/routes.js
@@ -3,19 +3,29 @@
 // https://prototype-kit.service.gov.uk/docs/create-routes
 //
 
-const govukPrototypeKit = require('govuk-prototype-kit')
-const router = govukPrototypeKit.requests.setupRouter()
+const govukPrototypeKit = require("govuk-prototype-kit");
+const router = govukPrototypeKit.requests.setupRouter();
 
 router.use((req, res, next) => {
-  res.locals.query = req.query
-  res.locals.path = req.path
-  next()
-})
+  res.locals.query = req.query;
+  res.locals.path = req.path;
+
+  // Set govBranded cookie if it doesn't exist
+  if (!req.cookies.govBranded) {
+    res.cookie("govBranded", "true", { maxAge: 900000, httpOnly: true });
+  }
+
+  // Make the cookie value available to templates
+  res.locals.govBranded = req.cookies.govBranded;
+  console.log("res.locals.govBranded:", res.locals.govBranded);
+
+  next();
+});
 
 // Sprint routes setup
 
-const sprint00 = require('./views/prototypes/sprint-00/_routes');
-router.use('/prototypes/sprint-00', sprint00);
+const sprint00 = require("./views/prototypes/sprint-00/_routes");
+router.use("/prototypes/sprint-00", sprint00);
 
-const sprint01 = require('./views/prototypes/sprint-01/_routes');
-router.use('/prototypes/sprint-01', sprint01);
+const sprint01 = require("./views/prototypes/sprint-01/_routes");
+router.use("/prototypes/sprint-01", sprint01);
