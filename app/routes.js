e76a33a57c9758b807c14235b8cc44432ca0b9b1
Lewis Morris <lewis@lewismorris.co.uk>
2025-06-19 14:35:46 +0100

    Modify unbranded logic to fire correctly (#1)

--- app/routes.js
+++ app/routes.js
@@ -15,9 +15,8 @@ router.use((req, res, next) => {
     res.cookie("govBranded", "true", { maxAge: 900000, httpOnly: true });
   }
 
-  // Make the cookie value available to templates
-  res.locals.govBranded = req.cookies.govBranded;
-  console.log("res.locals.govBranded:", res.locals.govBranded);
+  // Make the cookie value available to templates - convert string to boolean, default to true if undefined
+  res.locals.govBranded = req.cookies.govBranded === undefined ? true : req.cookies.govBranded === "true";
 
   next();
 });
@@ -29,3 +28,11 @@ router.use("/prototypes/sprint-00", sprint00);
 
 const sprint01 = require("./views/prototypes/sprint-01/_routes");
 router.use("/prototypes/sprint-01", sprint01);
+
+router.use("/switch-branding", (req, res) => {
+  res.cookie("govBranded", !res.locals.govBranded, {
+    maxAge: 900000,
+    httpOnly: true,
+  });
+  res.redirect(req.query.redirectTo || "/");
+});
