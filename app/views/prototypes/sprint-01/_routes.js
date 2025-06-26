7618bc56f797c6132e38517f8e960d952d81c2e0
Lewis Morris <lewis@lewismorris.co.uk>
2025-06-26 15:49:33 +0100

    Add basic chatbot flow

--- app/views/prototypes/sprint-01/_routes.js
+++ app/views/prototypes/sprint-01/_routes.js
@@ -1,32 +1,33 @@
 //const express = require('express')
 //const router = new express.Router()
 
-const govukPrototypeKit = require('govuk-prototype-kit')
-const router = govukPrototypeKit.requests.setupRouter()
+const govukPrototypeKit = require("govuk-prototype-kit");
+const router = govukPrototypeKit.requests.setupRouter();
 
 // Add your routes above the module.exports line
 
-router.post('/', function (req, res) {
-    res.redirect('how-are-you-feeling');
-  });
-  
-
-router.post('/eng-wal-private-renting', function (req, res) {
-    if (req.session.data['ans'] == 'Yes') {
-        res.redirect('what-do-you-need-help-with');
-    }
-    else if (req.session.data['ans'] == 'No') {
-        res.redirect('not-private-renting-within-eng-or-wal');
-    }
+router.post("/", function (req, res) {
+  res.redirect("how-are-you-feeling");
 });
 
-router.post('/what-do-you-need-help-with', function (req, res) {
-    if (req.session.data['help'] == 'con') {
-        res.redirect('contractual');
+router.post("/eng-wal-private-renting", function (req, res) {
+  if (req.session.data["ans"] == "Yes") {
+    if (req.cookies.chatbot) {
+      res.redirect("chatbot");
+    } else {
+      res.redirect("what-do-you-need-help-with");
     }
-    else if (req.session.data['help'] == 'main') {
-        res.redirect('maintanence-and-repairs');
-    }
+  } else if (req.session.data["ans"] == "No") {
+    res.redirect("not-private-renting-within-eng-or-wal");
+  }
 });
 
-module.exports = router
+router.post("/what-do-you-need-help-with", function (req, res) {
+  if (req.session.data["help"] == "con") {
+    res.redirect("contractual");
+  } else if (req.session.data["help"] == "main") {
+    res.redirect("maintanence-and-repairs");
+  }
+});
+
+module.exports = router;
