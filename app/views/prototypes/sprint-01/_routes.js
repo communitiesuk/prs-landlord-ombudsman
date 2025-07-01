61c7a407609cca76f76bcbe1615b3a0bd3acd3e0
Karl Parton <karlparton@gmail.com>
2025-07-01 15:09:39 +0100

    Code tidy.

--- app/views/prototypes/sprint-01/_routes.js
+++ app/views/prototypes/sprint-01/_routes.js
@@ -8,47 +8,48 @@ const router = govukPrototypeKit.requests.setupRouter();
 
 // Multiple
 
-router.post(
-  "/multiple/are-you-renting-in-england-or-wales",
-  function (req, res) {
-    if (req.session.data["renting-in-england-or-wales"] == "Yes") {
-      res.redirect("what-do-you-need-help-with");
-    } else if (req.session.data["renting-in-england-or-wales"] == "No") {
-      res.redirect("cannot-use-service");
-    }
+router.post("/multiple/are-you-renting-in-england-or-wales", function (req, res) {
+  if (req.session.data["rentingEnglandOrWales"] == "yes") {
+    res.redirect("what-do-you-need-help-with");
+  } else if (req.session.data["rentingEnglandOrWales"] == "no") {
+    res.redirect("cannot-use-service");
   }
-);
+});
 
 router.post("/multiple/what-do-you-need-help-with", function (req, res) {
-  if (req.session.data["need-help-with"] == "contractual") {
-    res.redirect("contractual");
-  } else if (req.session.data["need-help-with"] == "maintanence") {
+  if (req.session.data["needHelpWith"] == "tenancy") {
+    res.redirect("problem-with-tenancy");
+  } else if (req.session.data["needHelpWith"] == "maintanence") {
     res.redirect("maintanence-and-repairs");
   }
 });
 
-router.post("/multiple/contractual", function (req, res) {
+router.post("/multiple/problem-with-tenancy", function (req, res) {
   res.redirect("landlord-legal-issues");
 });
 
-router.post("/multiple/landlord-legal-issues", function (req, res) {
-  res.redirect("triage-results");
+router.post("/multiple/maintanence-and-repairs", function (req, res) {
+  res.redirect("utility-problem");
+});
+
+router.post("/multiple/utility-problem", function (req, res) {
+  res.redirect("next-steps");
 });
 
 // Single
 
 router.post("/single/are-you-renting-in-england-or-wales", function (req, res) {
-  if (req.session.data["renting-in-england-or-wales"] == "Yes") {
+  if (req.session.data["rentingEnglandOrWales"] == "yes") {
     res.redirect("what-do-you-need-help-with");
-  } else if (req.session.data["renting-in-england-or-wales"] == "No") {
+  } else if (req.session.data["rentingEnglandOrWales"] == "no") {
     res.redirect("cannot-use-service");
   }
 });
 
 router.post("/single/what-do-you-need-help-with", function (req, res) {
-  if (req.session.data["need-help-with"] == "contractual") {
+  if (req.session.data["needHelpWith"] == "tenancy") {
     res.redirect("problem-with-tenancy");
-  } else if (req.session.data["need-help-with"] == "maintanence") {
+  } else if (req.session.data["needHelpWith"] == "maintanence") {
     res.redirect("maintanence-and-repairs");
   }
 });
