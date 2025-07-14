96bdbad19e3d61f71bf1659651adbfe88febdae3
Lewis Morris <lewis@lewismorris.co.uk>
2025-07-14 17:28:04 +0100

    Add basic LLM behaviour

--- app/views/prototypes/sprint-02/_routes.js
+++ app/views/prototypes/sprint-02/_routes.js
@@ -1,6 +1,5 @@
 //const express = require('express')
 //const router = new express.Router()
-
 const govukPrototypeKit = require("govuk-prototype-kit");
 const router = govukPrototypeKit.requests.setupRouter();
 
