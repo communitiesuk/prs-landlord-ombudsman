39ff7ed9e4ce6d5ee87f084b9766102db140d4f1
Lewis Morris <lewis@lewismorris.co.uk>
2025-07-14 15:02:06 +0100

    Build out LLM prototype

--- app/views/prototypes/sprint-02/_routes.js
+++ app/views/prototypes/sprint-02/_routes.js
@@ -6,21 +6,25 @@ const router = govukPrototypeKit.requests.setupRouter();
 
 // Add your routes above the module.exports line
 
-// Chatbot
-
-router.post(
-  "/chatbot/are-you-renting-in-england-or-wales",
-  function (req, res) {
-    if (req.session.data["renting-in-england-or-wales"] == "Yes") {
-      res.redirect("ask");
-    } else if (req.session.data["renting-in-england-or-wales"] == "No") {
-      res.redirect("cannot-use-service");
-    }
+// LLM
+router.post("/llm/are-you-renting-in-england-or-wales", function (req, res) {
+  if (req.session.data["rentingEnglandOrWales"] == "yes") {
+    res.redirect("what-do-you-need-help-with");
+  } else if (req.session.data["rentingEnglandOrWales"] == "no") {
+    res.redirect("cannot-use-service");
   }
-);
+});
 
-router.post("/chatbot/ask", function (req, res) {
-  res.redirect("answer");
+router.post("/llm/what-do-you-need-help-with", function (req, res) {
+  res.redirect("have-you-spoken-to-anyone");
+});
+
+router.post("/llm/have-you-spoken-to-anyone", function (req, res) {
+  if (req.session.data["haveYouSpokenToAnyone"] == "no") {
+    res.redirect("more-information");
+  } else if(req.session.data["haveYouSpokenToAnyone"] === 'landlord' || req.session.data["haveYouSpokenToAnyone"] === 'localAuthority' || req.session.data["haveYouSpokenToAnyone"] === 'charity') {
+    res.redirect("contact-details");
+  }
 });
 
 module.exports = router;
