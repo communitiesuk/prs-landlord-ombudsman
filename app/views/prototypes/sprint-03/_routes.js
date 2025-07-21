ee3975d6ba401abcd74a0242872cda5bccfed4fc
Karl Parton <karlparton@gmail.com>
2025-07-21 10:48:14 +0100

    Created sprint 3 dir

---
+++ app/views/prototypes/sprint-03/_routes.js
@@ -0,0 +1,14 @@
+//const express = require('express')
+//const router = new express.Router()
+
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+// Add your routes above the module.exports line
+
+router.post('/', function (req, res) {
+    res.redirect('');
+  });
+  
+
+module.exports = router
