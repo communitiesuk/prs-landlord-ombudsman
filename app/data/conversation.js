a8628e8d825de12d01ac41a8796cb41c9a6f8e9d
Karl Parton <karlparton@gmail.com>
2025-07-24 09:10:51 +0100

    Content updates.

--- app/data/conversation.js
+++ app/data/conversation.js
@@ -2,29 +2,65 @@ module.exports = {
   messages: [
     {
       sender: 'tenant',
-      text: 'Hello. I\'m messaging you to tell you that a pipe has burst in the flat.'
+      text: 'Hello. I\'m messaging you to tell you that a pipe has burst in the flat.',
+      datestamp: '5 May 2015'
     },
     {
       sender: 'landlord',
-      text: 'Hello tenant. Oh no. Let\'s get that repaired as soon as possible. Can you give me some more information.'
+      text: 'Hello. Oh no. Let\'s get that repaired as soon as possible. Can you give me some more information.',
+      datestamp: '5 May 2015'
     },
     {
       sender: 'tenant',
       text: 'Sure. Here\'s a pic of the burst pipe. It burst on Sunday evening around 8pm. Not long after I started a load of washing.',
-      // The path starts from the assets folder
-      imageUrl: '/public/images/pipe.jpg'
+      imageUrl: '/public/images/pipe.jpg',
+      datestamp: '5 May 2015'
     },
     {
       sender: 'landlord',
-      text: 'Thanks for the information. I will get in touch with a plumber ASAP to get that fixed for you. I\'ll be in touch with the details shortly.'
+      text: 'Thanks for the information. I will get in touch with a plumber ASAP to get that fixed for you. I\'ll be in touch with the details shortly.',
+      datestamp: '5 May 2015'
+    },
+    {
+      sender: 'tenant',
+      text: 'Hi, just following up on this. It\'s been 3 days and I haven\'t heard anything about the plumber. The leak is still ongoing.',
+      datestamp: '8 May 2015'
+    },
+    {
+      sender: 'landlord',
+      text: 'Sorry for the delay, I\'ve been very busy. The plumber I normally use is unavailable until next week.',
+      datestamp: '8 May 2015'
+    },
+    {
+      sender: 'tenant',
+      text: 'That\'s not acceptable, this is an emergency repair. The water is damaging the floorboards now. Can you please call another plumber?',
+      datestamp: '8 May 2015'
+    },
+    {
+      sender: 'landlord',
+      text: 'I only use my trusted tradespeople. I\'ll see if he can move things around.',
+      datestamp: '9 May 2015'
+    },
+    {
+      sender: 'tenant',
+      text: 'This needs to be resolved. Under my tenancy agreement, you are required to carry out emergency repairs promptly. If I don\'t have a confirmed appointment for a plumber by the end of tomorrow, I will have to explore other options.',
+      datestamp: '16 May 2015'
+    },
+    {
+      sender: 'landlord',
+      text: 'I\'ll do what I can.',
+      datestamp: '17 May 2015'
+    },
+    // --- NEW MESSAGE ADDED HERE ---
+    {
+      sender: 'tenant',
+      text: 'I have spoken to the local authority housing team and they have advised me that you are responsible for this repair.',
+      datestamp: '24 May 2015'
+    },
+    {
+      sender: 'tenant',
+      text: 'Given your refusal to treat this emergency with the required urgency, I\'m left with no choice. I am going to register the issue with the ombudsman.',
+      datestamp: '28 May 2015'
     }
   ]
 }
-
- 
-
- 
-
- 
-
- 
